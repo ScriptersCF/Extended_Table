cbb7dbc84bc4f18d1656769d22a7a40c4fc3cc3a
Matthew CntKillMe <willyoumeryme@gmail.com>
2020-01-26 10:28:21 -0800

    support cyclic tables

--- funcs/deepcopy.lua
+++ funcs/deepcopy.lua
@@ -1,11 +1,22 @@
 -- | deepcopy | completely copies a table
 
 ---@param tbl table "the table to be deep copied"
+---@param seen table "the set of tables previously processed, optional"
 ---@return table "the new deep copy of the provided table"
-local function deepcopy(tbl)
+local function deepcopy(tbl, seen)
   local ret = {}
+  seen = seen or {}
+  seen[tbl] = ret
+
   for k, v in pairs(tbl) do
-    ret[k] = type(v) == 'table' and deepcopy(v) or v
+    if type(v) == 'table' then
+      if not seen[v] then
+        seen[v] = deepcopy(v, seen)
+      end
+      ret[k] = seen[v]
+    else
+      ret[k] = v
+    end
   end
   return ret
 end
